4695df21a744080816b856221836bcdcc11741ca
Neerajgowda2611 <112490279+Neerajgowda2611@users.noreply.github.com>
2024-09-26 20:02:59 +0530

    Update StackNavigator.tsx

--- frontend/src/navigation/StackNavigator.tsx
+++ frontend/src/navigation/StackNavigator.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '/home/silent/saramsha/frontend/src/screens/HomeScreen.tsx';
-import RecordAudioScreen from '/home/silent/saramsha/frontend/src/screens/RecordAudioScreen.tsx';
-import UploadAudioScreen from '/home/silent/saramsha/frontend/src/screens/UploadAudioScreen.tsx';
-import PlayAudioScreen from '/home/silent/saramsha/frontend/src/screens/PlayAudioScreen.tsx'; 
-import ResultsScreen from '../screens/ResultsScreen';
+import HomeScreen from '/home/silent/saramsha-here/frontend/src/screens/HomeScreen.tsx';
+import RecordAudioScreen from '/home/silent/saramsha-here/frontend/src/screens/RecordAudioScreen.tsx';
+import UploadAudioScreen from '/home/silent/saramsha-here/frontend/src/screens/UploadAudioScreen.tsx';
+import PlayAudioScreen from '/home/silent/saramsha-here/frontend/src/screens/PlayAudioScreen.tsx'; 
+import ResultsScreen from '/home/silent/saramsha-here/frontend/src/screens/ResultsScreen.tsx';
+import HistoryScreen from '../screens/HistoryScreen';
+import ViewScreen from '/home/silent/saramsha-here/frontend/src/screens/ViewScreen.tsx';
+
+
 // import UploadPlayAudioScreen from '../screens/UploadPlayAudioScreen';
 
 const Stack = createStackNavigator();
@@ -17,6 +21,10 @@ const StackNavigator = () => {
       <Stack.Screen name="UploadAudio" component={UploadAudioScreen} />
       <Stack.Screen name="PlayAudio" component={PlayAudioScreen} /> 
       <Stack.Screen name="ResultsScreen" component={ResultsScreen} />
+      <Stack.Screen name="HistoryScreen" component={HistoryScreen} />
+      <Stack.Screen name="ViewScreen" component={ViewScreen} />
+      {/* <Stack.Screen name="ViewScreen" component={ViewScreen} /> */}
+
       {/* <Stack.Screen name="UploadPlayAudioScreen" component={UploadPlayAudioScreen} /> */}
     </Stack.Navigator>
   );
